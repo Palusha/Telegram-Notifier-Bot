e6953fa1c1b285fe4e60217d2d8a07753e5f9d2d
Palusha <monday13081@gmail.com>
2020-10-03 16:37:54 +0300

    yeah, that's it

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import config
 import threading
 import time
+import logging
+import requests
 
 week = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
 bot = telebot.TeleBot(config.token)
@@ -78,13 +80,9 @@ def notify():
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         temp = current_time.split(':')
-        forward_time_min = int(temp[0]) * 3600 + int(temp[1]) * 60 + int(temp[2]) + 600
-        forward_time = ('0' + str(forward_time_min // 3600))[-2:] + ':' + (
-                '0' + str(forward_time_min % 3600 // 60))[-2:] + ':' + ('0' + str(forward_time_min % 3600 % 60))[-2:]
-
-        print(current_time)
-        print(forward_time)
-        print()
+        forward_time_sec = int(temp[0]) * 3600 + int(temp[1]) * 60 + int(temp[2]) + 600
+        forward_time = ('%02d:%02d:%02d' % (
+            forward_time_sec // 3600, forward_time_sec % 3600 // 60, forward_time_sec % 3600 % 60))
 
         if current_time == "00:00:00":
             first_subgroup_schedule = first_subgroup_data[week[datetime.now().weekday()]]
@@ -92,30 +90,64 @@ def notify():
 
         if forward_time in first_subgroup_schedule:
             for i in users["subgroup1"]:
-                bot.send_message(i, f'Пара "{first_subgroup_schedule[forward_time]["name"]}" '
-                                    f'почнеться через 10 хвилин \nПосилання: '
-                                    f'{first_subgroup_schedule[forward_time]["link"]}')
+                try:
+                    bot.send_message(i, f'Пара "{first_subgroup_schedule[forward_time]["name"]}" '
+                                        f'почнеться через 10 хвилин \nПосилання: '
+                                        f'{first_subgroup_schedule[forward_time]["link"]}')
+                except telebot.apihelper.ApiTelegramException:
+                    users["subgroup1"].remove(i)
+
+            with open('users.json', 'w') as js:
+                json.dump(users, js)
 
         if forward_time in second_subgroup_schedule:
             for i in users["subgroup2"]:
-                bot.send_message(i, f'Пара "{second_subgroup_schedule[forward_time]["name"]}" '
-                                    f'почнеться через 10 хвилин \nПосилання: '
-                                    f'{second_subgroup_schedule[forward_time]["link"]}')
+                try:
+                    bot.send_message(i, f'Пара "{second_subgroup_schedule[forward_time]["name"]}" '
+                                        f'почнеться через 10 хвилин \nПосилання: '
+                                        f'{second_subgroup_schedule[forward_time]["link"]}')
+                except telebot.apihelper.ApiTelegramException:
+                    users["subgroup2"].remove(i)
+
+            with open('users.json', 'w') as js:
+                json.dump(users, js)
 
         if current_time in first_subgroup_schedule:
             for i in users["subgroup1"]:
-                bot.send_message(i, f'Пара: "{first_subgroup_schedule[current_time]["name"]}" почалась. '
-                                    f'\nПосилання: {first_subgroup_schedule[current_time]["link"]}')
+                try:
+                    bot.send_message(i, f'Пара: "{first_subgroup_schedule[current_time]["name"]}" почалась. '
+                                        f'\nПосилання: {first_subgroup_schedule[current_time]["link"]}')
+                except telebot.apihelper.ApiTelegramException:
+                    users["subgroup1"].remove(i)
+
+            with open('users.json', 'w') as js:
+                json.dump(users, js)
 
         if current_time in second_subgroup_schedule:
             for i in users["subgroup2"]:
-                bot.send_message(i, f'Пара: "{second_subgroup_schedule[current_time]["name"]}" почалась.'
-                                    f'\nПосилання: {second_subgroup_schedule[current_time]["link"]}')
+                try:
+                    bot.send_message(i, f'Пара: "{second_subgroup_schedule[current_time]["name"]}" почалась.'
+                                        f'\nПосилання: {second_subgroup_schedule[current_time]["link"]}')
+                except telebot.apihelper.ApiTelegramException:
+                    users["subgroup2"].remove(i)
+
+            with open('users.json', 'w') as js:
+                json.dump(users, js)
 
         time.sleep(1)
 
 
-thr = threading.Thread(target=notify)
-thr.start()
+url = "http://www.google.com"
 
-bot.polling(none_stop=True)
+while True:
+    thr = threading.Thread(target=notify, daemon=True)
+    thr.start()
+
+    try:
+        requests.get(url, timeout=5)
+        bot.polling(none_stop=True)
+
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        logging.error(exception)
+        print("No internet connection.")
+        time.sleep(5)
